dd127946c9463655eeabfdfdee6a7f8ed0c054e9
Lucas Renato Fernandes da Silva <lucas.silva@skyinformtica.com.br>
2026-06-03 09:27:40 -0300

    Melhorando configuração do WriterMode

--- lua/writer-mode.lua
+++ lua/writer-mode.lua
@@ -9,13 +9,27 @@ function ToggleWriterMode()
         vim.cmd('set wrap')
         vim.cmd('set linebreak')
         vim.cmd('set nolist')
+        vim.keymap.set({ "n", "v" }, "j", "gj", { noremap = true, silent = true })
+        vim.keymap.set({ "n", "v" }, "k", "gk", { noremap = true, silent = true })
+        vim.keymap.set({ "n", "v" }, "<Down>", "gj", { noremap = true, silent = true })
+        vim.keymap.set({ "n", "v" }, "<Up>", "gk", { noremap = true, silent = true })
+        vim.keymap.set({ "n", "v" }, "$", "g$", { noremap = true, silent = true })
+        vim.keymap.set({ "n", "v" }, "_", "g^", { noremap = true, silent = true })
+        vim.keymap.set({ "n", "v" }, "0", "g0", { noremap = true, silent = true })
     else
         vim.cmd('set nowrap')
         vim.cmd('set nolinebreak')
         vim.cmd('set list')
+        vim.keymap.del({ "n", "v" }, "j")
+        vim.keymap.del({ "n", "v" }, "k")
+        vim.keymap.del({ "n", "v" }, "<Down>")
+        vim.keymap.del({ "n", "v" }, "<Up>")
+        vim.keymap.set({ "n", "v" }, "$")
+        vim.keymap.set({ "n", "v" }, "_")
+        vim.keymap.set({ "n", "v" }, "0")
     end
 end
 
 vim.api.nvim_create_user_command("WriterMode", ToggleWriterMode, {})
 
-vim.keymap.set("n", "<leader>wm", ToggleWriterMode, { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>wm", ToggleWriterMode, { noremap = true, silent = true })
