3b025d60361db0126c7f47614428cf4ee25798f6
Lucas Renato Fernandes da Silva <lucas.silva@skyinformtica.com.br>
2026-06-05 11:30:02 -0300

    Criação de comando para pesquisar no código com base no texto selecionado

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,5 +8,33 @@ return {
         vim.keymap.set('n', '<leader>F', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>p', builtin.find_files, {})
         vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+
+        local function get_visual_selection()
+            local s_start = vim.fn.getpos "."
+            local s_end = vim.fn.getpos "v"
+            return vim.fn.getregion(s_start, s_end)
+        end
+
+        local function search_selected_text(command)
+            local selected_lines = get_visual_selection()
+            if (#selected_lines > 1) then
+                vim.notify("Cannot grep multiple lines", vim.log.levels.WARN)
+                return
+            end
+            local selected = table.concat(selected_lines, '\n')
+            vim.notify(selected_lines)
+            command({
+                default_text = selected,
+                initial_mode = "insert"
+            })
+        end
+
+        vim.keymap.set('v', '<leader>f', function()
+            search_selected_text(builtin.current_buffer_fuzzy_find)
+        end)
+
+        vim.keymap.set('v', '<leader>F', function()
+            search_selected_text(builtin.live_grep)
+        end)
     end
 }
